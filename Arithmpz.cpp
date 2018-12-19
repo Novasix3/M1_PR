4c1bd35f6e5ac6b04016c2bd2e2b9298e2ffc65e
Valeri Yoann <novasix3@netcourrier.com>
2018-12-19 09:52:01 +0000

    Replace Arithmpz.cpp

--- Arithmpz.cpp
+++ Arithmpz.cpp
@@ -70,20 +70,49 @@ namespace model{
 	}
 	
 	vector<mpz_class> mult(const vector<mpz_class> a,const vector<mpz_class> b){
-		vector<mpz_class> res(a.size() + b.size());
-		for(int i = 0; i < a.size(); ++i){
-			
-			for(int j = 0; j < b.size(); ++j){
+		cout << "on a A = " << endl;
+			for(int l = 0; l < a.size(); ++l)
+				cout << a[l] << endl;
+		cout << "et B = " << endl;
+			for(int l = 0; l < b.size(); ++l)
+				cout << b[l] << endl;
+		if(a.size() == 1){
+			mult(b, a[0]);
+			return b;
+		}else if(b.size() == 1){
+			mult(a, b[0]);
+			return a;
+		}
+		int cap1 = a.size(), cap2 = b.size();
+		cout << "cap1 = " << cap1 << endl;
+		cout << "cap2 = " << cap2 << endl;
+		while(cap1 > 0 && a[cap1-1] == 0) 
+			--cap1;
+		while(cap2 > 0 && b[cap2-1] == 0) 
+			--cap2;
+		cout << "cap1 after = " << cap1 << endl;
+		cout << "cap2 after = " << cap2 << endl;
+		vector<mpz_class> res(cap1 + cap2 - 1);
+		for(int i = 0; i < cap1; ++i){
+			for(int j = 0; j < cap2; ++j){
 				res[i+j] += a[i]*b[j];
 			}
 		}
+		
+		cout << "en sortant de la mult, on a res = " << endl;
+			for(int l = 0; l < res.size(); ++l)
+				cout << res[l] << endl;
 		
 		return res;
 	}
 	
 	void mult(vector<mpz_class> a, const mpz_class b){
+		cout << "on a A = " << endl;
+			for(int l = 0; l < a.size(); ++l)
+				cout << a[l] << endl;
+		
 		for(int i = 0; i < a.size(); ++i)
-			a[i] = a[i] * b;
+			a[i] *= b;
 		
 	}
 	
